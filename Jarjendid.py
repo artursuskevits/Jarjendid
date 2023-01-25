fa3e12b2670e446a4f65e3310e8e77bc1efe4e72
opilane <opilane@HP-CZC2349HTN>
2023-01-25 12:31:22 +0200

    Add project files.

---
+++ Jarjendid.py
@@ -0,0 +1,42 @@
+#spisok = [] #pustoy spisok
+#numbers = [1,2,3,4,5]
+#abc=["abc","b","c"]
+#slovo = "programmeerimine"
+#slovo_list=list(slovo)
+#print(slovo)
+#print(slovo_list)
+#while True:
+#    print("1-add letter in list")
+#    print("2-skleit spiski\n3 - add letter on i pose\n4 - delete element ")
+#    valik=int(input())
+#    if valik ==1:
+#        a=input("print letter")
+#        slovo_list.append(a)
+#        print(f"we add {a} in ",slovo_list)
+#    elif valik==2:
+#        slovo_list.extend(abc)
+#        print(slovo_list)
+#    elif valik ==3:
+#        a=input ("add letter whitch you want add in list")
+#        i=(int(input("add number of pose where you want add letter")))"
+#        slovo_list.insert(i-1,a)
+#        print(slovo_list)
+#    elif valik == 4:
+#        a=input ("add letter whitch you want delete in list")
+#        n=slovo_list.count(a)
+#        if  n > 0:
+#            print(n)
+#            for z in range (n):
+#                slovo_list.remove(a)
+#        else :
+#            print(f"we not found {a}")
+#        print(slovo_list) 
+
+#my 10 rasks
+
+txt = "i hate world"
+
+print("all words starts from big letter")
+
+x = txt.title()
+print(x)
